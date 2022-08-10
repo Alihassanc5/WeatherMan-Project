be2b3ba26a9a8ac4faf12fc650b98708861497db
Alihassanc5 <Alihassan01264@gmail.com>
2022-08-10 16:47:17 +0500

    I have fixed all issues except modules

--- calculations.py
+++ calculations.py
@@ -2,90 +2,138 @@ from calendar import month_abbr, month_name
 from csv import DictReader
 
 
-def weather_readings(files, mode):
-    readings = []
+def read_weather_files(weather_files):
+    weather_readings = []
 
-    for file in files:
-        with open(file, newline='') as opened_file:
-            reader = DictReader(opened_file, skipinitialspace = True)
-            
-            for reading in reader:
-                weather_reading = parse_reading(reading, mode)
-                readings.append(weather_reading)
+    for weather_file in weather_files:
+        with open(weather_file, newline='') as opened_weather_file:
+            weather_reader = DictReader(opened_weather_file,
+                                        skipinitialspace=True)
 
-    return readings
+            for weather_reading in weather_reader:
+                weather_readings.append(weather_reading)
+
+    return weather_readings
 
 
-def parse_reading(reading, operation):
-    max_temperature = -1 if reading['Max TemperatureC'] == '' else int(reading['Max TemperatureC'])
-    min_temperature = -1 if reading['Min TemperatureC'] == '' else int(reading['Min TemperatureC'])
+def parse_weather_reading(weather_reading):
+    max_temperature = -1 if weather_reading['Max TemperatureC'] == '' \
+        else int(weather_reading['Max TemperatureC'])
+    min_temperature = -1 if weather_reading['Min TemperatureC'] == '' \
+        else int(weather_reading['Min TemperatureC'])
+    max_humidity = -1 if weather_reading['Max Humidity'] == '' \
+        else int(weather_reading['Max Humidity'])
+    mean_humidity = -1 if weather_reading['Mean Humidity'] == '' \
+        else int(weather_reading['Mean Humidity'])
+    timezone = 'PKT' if 'PKT' in weather_reading else 'PKST'
+    date = list(map(int, weather_reading[timezone].split('-')))
 
-    weather_reading = {'Max TemperatureC': max_temperature, 'Min TemperatureC': min_temperature}
-    
-    if operation == 'yearly operations':
-        timezone = 'PKT' if 'PKT' in reading else 'PKST'
-        date = list(map(int,reading[timezone].split('-')))
-        max_humidity = -1 if reading['Max Humidity'] == '' else int(reading['Max Humidity'])
-        weather_reading['date'] = {'Month': date[1], 'Day': date[2]}
-        weather_reading['Max Humidity'] = max_humidity
-    
-    if operation == 'monthly operations':
-        mean_humidity = -1 if reading['Mean Humidity'] == '' else int(reading['Mean Humidity'])
-        weather_reading['Mean Humidity'] = mean_humidity
+    parsed_weather_reading = {
+                              'Max TemperatureC': max_temperature,
+                              'Min TemperatureC': min_temperature,
+                              'date': {'Month': date[1], 'Day': date[2]},
+                              'Mean Humidity': mean_humidity,
+                              'Max Humidity': max_humidity
+                             }
 
-    return weather_reading
+    return parsed_weather_reading
+
+
+def parse_weather_readings(weather_readings):
+    parsed_weather_readings = []
+
+    for weather_reading in weather_readings:
+        parsed_weather_reading = parse_weather_reading(weather_reading)
+        parsed_weather_readings.append(parsed_weather_reading)
+
+    return parsed_weather_readings
 
 
 def calculate_mean_values(weather_readings):
-    highest_temperature_sum = sum(weather_reading['Max TemperatureC'] for weather_reading in weather_readings if weather_reading['Max TemperatureC'] != -1)
-    lowest_temperature_sum = sum(weather_reading['Min TemperatureC'] for weather_reading in weather_readings if weather_reading['Min TemperatureC'] != -1)
-    mean_humidity_sum = sum(weather_reading['Mean Humidity'] for weather_reading in weather_readings if weather_reading['Mean Humidity'] != -1)
-    
-    average_highest_temperature = highest_temperature_sum / len(weather_readings)
-    average_lowest_temperature = lowest_temperature_sum / len(weather_readings)
-    average_mean_humidity = mean_humidity_sum / len(weather_readings) 
+    average_max_temperature = sum(
+                                  reading['Max TemperatureC']
+                                  for reading in weather_readings
+                                  if reading['Max TemperatureC'] != -1
+                              ) / len(weather_readings)
+    average_min_temperature = sum(
+                                  reading['Min TemperatureC']
+                                  for reading in weather_readings
+                                  if reading['Min TemperatureC'] != -1
+                              ) / len(weather_readings)
+    average_mean_humidity = sum(
+                                reading['Mean Humidity']
+                                for reading in weather_readings
+                                if reading['Mean Humidity'] != -1
+                            ) / len(weather_readings)
 
-    return average_highest_temperature, average_lowest_temperature, average_mean_humidity
+    return average_max_temperature, average_min_temperature, \
+        average_mean_humidity
 
 
 def display_bar_charts(weather_readings):
-    for day,weather_reading in enumerate(weather_readings, start = 1):
+    for day, weather_reading in enumerate(weather_readings, start=1):
         highest_temperature = weather_reading['Max TemperatureC']
         lowest_temperature = weather_reading['Min TemperatureC']
-        
-        print(f"{day:02d}", end = '')
-        if lowest_temperature >= 0:
-            print(f"\033[1;36;40m{lowest_temperature * '+'}\033[0;37;40m", end = '')
 
+        print(f"{day:02d}", end='')
+        if lowest_temperature >= 0:
+            print(f"\033[1;36;40m{lowest_temperature * '+'}\033[0;37;40m", end='')
         if highest_temperature >= 0:
-            print(f"\033[1;31;40m{highest_temperature * '+'}\033[0;37;40m", end = ' ')
-        
+            print(f"\033[1;31;40m{highest_temperature * '+'}\033[0;37;40m", end=' ')
         if lowest_temperature != -1 and highest_temperature != -1:
-           print(f"{lowest_temperature:2d}C - {highest_temperature:2d}C",end = '')
-        print()        
+            print(f"{lowest_temperature:2d}C - {highest_temperature:2d}C", end='')
+        print()
 
 
 def generate_report(weather_readings, operation):
     if operation == 'yearly operations':
-        filtered_readings = list(filter(lambda reading: reading['Min TemperatureC'] != -1, weather_readings))
-        lowest_temp_weather = min(filtered_readings, key = lambda reading: reading['Min TemperatureC'])
-        highest_temp_weather = max(filtered_readings, key = lambda reading: reading['Max TemperatureC'])
-        most_humid_weather = max(filtered_readings, key = lambda reading: reading['Max Humidity'])
+        filtered_readings = list(
+                                filter(
+                                    lambda reading:
+                                    reading['Min TemperatureC'] != -1,
+                                    weather_readings
+                                )
+                            )
+        lowest_temp_weather = min(
+                                  filtered_readings,
+                                  key=lambda reading:
+                                  reading['Min TemperatureC']
+                              )
+        highest_temp_weather = max(
+                                   filtered_readings,
+                                   key=lambda reading:
+                                   reading['Max TemperatureC']
+                               )
+        most_humid_weather = max(
+                                 filtered_readings,
+                                 key=lambda reading:
+                                 reading['Max Humidity']
+                             )
 
-        print(f"Highest: {highest_temp_weather['Max TemperatureC']}C on {month_name[highest_temp_weather['date']['Month']]} {highest_temp_weather['date']['Day']}")
-        print(f"Lowest: {lowest_temp_weather['Min TemperatureC']}C on {month_name[lowest_temp_weather['date']['Month']]} {lowest_temp_weather['date']['Day']}")
-        print(f"Humidity: {most_humid_weather['Max Humidity']}% on {month_name[most_humid_weather['date']['Month']]} {most_humid_weather['date']['Day']}")
-
+        print(
+            f"Highest: {highest_temp_weather['Max TemperatureC']}C on"
+            f"{month_name[highest_temp_weather['date']['Month']]} "
+            f"{highest_temp_weather['date']['Day']}"
+        )
+        print(
+            f"Lowest: {lowest_temp_weather['Min TemperatureC']}C on "
+            f"{month_name[lowest_temp_weather['date']['Month']]} "
+            f"{lowest_temp_weather['date']['Day']}"
+        )
+        print(
+            f"Humidity: {most_humid_weather['Max Humidity']}% on "
+            f"{month_name[most_humid_weather['date']['Month']]} "
+            f"{most_humid_weather['date']['Day']}"
+        )
     elif operation == 'monthly operations':
         highest_average, lowest_average, average_mean_humidity = calculate_mean_values(weather_readings)
         print(f"Highest Average: {highest_average:.2f}C")
         print(f"Lowest Average: {lowest_average:.2f}C".format())
         print(f"Average Mean Humidity: {average_mean_humidity:.2f}%")
-
     elif operation == 'monthly bar charts':
         display_bar_charts(weather_readings)
-
     else:
-        raise Exception('Error in report generation!',operation,'is unknown.')
+        raise Exception('Error in report generation!',
+                        operation, 'is unknown.')
 
     print('-' * 30, '\n')
--- main.py
+++ main.py
@@ -1,49 +1,61 @@
 from os import path
 import argparse
 
-from calculations import *  
+from calculations import *
+
 
 def main():
     try:
         parser = argparse.ArgumentParser()
-        parser.add_argument("path_to_files", type = str, help = "Path of files")
-        parser.add_argument("-e", "--mode_e", default=[], action="append", help="To process file in E mode")
-        parser.add_argument("-a", "--mode_a", default=[], action="append", help="To process file in A mode")
-        parser.add_argument("-c", "--mode_c", default=[], action="append", help="To process file in C mode")
+        parser.add_argument("path_to_files", type=str, help="Path of files")
+        parser.add_argument("-e", "--mode_e", default=[], action="append", help="To do yearly operations")
+        parser.add_argument("-a", "--mode_a", default=[], action="append", help="To do monthly operations")
+        parser.add_argument("-c", "--mode_c", default=[], action="append", help="To display bar charts of a month")
         args = parser.parse_args()
 
-        operations = {"yearly operations": args.mode_e, "monthly operations": args.mode_a, "monthly bar charts": args.mode_c} 
-        
+        operations = {
+                      "yearly operations": args.mode_e,
+                      "monthly operations": args.mode_a,
+                      "monthly bar charts": args.mode_c
+        }
+
         for operation, dates in operations.items():
             for date in dates:
-                file_names = []
-                file_paths = []
+                weather_file_names = []
+                weather_files = []
 
-                if operation == 'monthly operations' or operation == 'monthly bar charts':
+                if (operation == 'monthly operations' or
+                        operation == 'monthly bar charts'):
                     year, month = date.split('/')
                     month = int(month)
                     print(month_name[month], year)
-                    
-                    file_name = "Murree_weather_" + year + "_" + month_abbr[month] + ".txt"
-                    file_names.append(file_name)            
+                    weather_file_name = ("Murree_weather_"
+                                         + year + "_"
+                                         + month_abbr[month]
+                                         + ".txt")
+                    weather_file_names.append(weather_file_name)
                 else:
                     for month_number in range(1, 13):
-                        file_name = "Murree_weather_" + date + "_" + month_abbr[month_number] + ".txt"
-                        file_names.append(file_name)
-                
-                for file_name in file_names:
-                    file_path = path.join(args.path_to_files, file_name)
-                    if path.exists(args.path_to_files):
-                        file_paths.append(file_path)
+                        weather_file_name = ("Murree_weather_"
+                                             + date + "_"
+                                             + month_abbr[month_number]
+                                             + ".txt")
+                        weather_file_names.append(weather_file_name)
 
-                readings = weather_readings(file_paths, operation)
-                generate_report(readings, operation)
+                for weather_file_name in weather_file_names:
+                    weather_file = path.join(args.path_to_files,
+                                             weather_file_name)
+                    if path.exists(args.path_to_files):
+                        weather_files.append(weather_file)
+
+                weather_readings = read_weather_files(weather_files)
+                parsed_weather_readings = parse_weather_readings(weather_readings)
+                generate_report(parsed_weather_readings, operation)
 
     except Exception as e:
         print('Exception Occured!!!')
         print(e)
-        
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-    
